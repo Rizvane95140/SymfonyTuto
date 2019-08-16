af8d76a2e8567a793b676d68b73ccd599cc17f53
Rizvane <rizvane95140@gmail.com>
2019-08-16 17:21:30 +0200

    filtre

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,13 +38,24 @@ class PropertyController extends AbstractController {
      *
      */
 
-    public function index(): Response {
+    public function index(PaginatorInterface $paginator, Request $request): Response
+    {
 
-        $properties = $this->repository->findAllVisible();
-        dump($properties);
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class,$search);
+        $form->handleRequest($request);
+
+
+        $properties = $paginator->paginate
+        ($this->repository->findAllVisibleQuery($search),
+           $request->query->getInt('page', 1),
+            12
+            );
 
         return $this->render('property/index.html.twig',  [
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'properties' => $properties,
+            'form' => $form->createView()
         ]);
     }
 
